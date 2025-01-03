c07f30c7b808f8cde3f016ba8ef83730f11f155d
Sourabha <thesks@gmail.com>
2025-01-02 19:14:36 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
-}
 
+function App({ videos, emptyHeading }) {
+  const count = videos.length;
+  let heading = emptyHeading;
+  if (count > 0) {
+    const noun = count > 1 ? 'Videos' : 'Video';
+    heading = count + ' ' + noun;
+  }
+  return (
+    <section>
+      <h2>{heading}</h2>
+      {videos.map(video =>
+        <Video key={video.id} video={video} />
+      )}
+    </section>
+  );
+}
 export default App;
